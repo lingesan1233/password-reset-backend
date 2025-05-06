51cde67322074082a21527ab6c1b0404c9f76be8
lingesan <sglingesan@gmail.com>
2025-05-06 18:55:54 +0530

    com

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { signup, login, resetPassword } = require('../controllers/authController');
-
-router.post('/signup', signup);
-router.post('/login', login);
-router.post('/reset-password', resetPassword);
-
-module.exports = router;
